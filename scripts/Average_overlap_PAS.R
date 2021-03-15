f4a07181d693ecc2c841373d94a83e43499acc28
YangLabPublic <78031537+YangLabPublic@users.noreply.github.com>
2021-03-15 14:18:30 +0800

    Update Average_overlap_PAS.R

--- scripts/Average_overlap_PAS.R
+++ scripts/Average_overlap_PAS.R
@@ -1,22 +1,47 @@
 options(warn = -1)
 #if(!require('Matrix')){install.packages('Matrix')}
+#if(!require('parallel')){install.packages('parallel')}
 args <- commandArgs(trailingOnly = TRUE)
 inputfile <- as.character(args[1])
 outfile <- as.character(args[2])
-t1<-Sys.time()
 rawcount <- read.table(inputfile, header=T)
-#rawcountbak <- rawcount
 colnames(rawcount) <- paste0(colnames(rawcount),"-1")
 rawcount <- Matrix::Matrix(as.matrix( rawcount ), sparse = TRUE)
-t2<-Sys.time()
 rawcount_fix <- rawcount
 allnames <- rownames(rawcount)
 overlapid <- grep(",",allnames)
+overlapname <- allnames[overlapid]
+multiplethreads <- TRUE
+
+if(multiplethreads == FALSE){
 for( id in overlapid ){
 	idn <- unlist(strsplit(allnames[id],split=","))
 	rawcount_fix[idn,] <- apply(rawcount_fix[c(idn,allnames[id]),],2,function(x){ idl <- length(x); y <- (x[-idl]+x[idl]/(idl-1)) ; return(y) })
 }
 rawcount_fix <- rawcount_fix[-overlapid,]
 rawcount_fix <- round(rawcount_fix,0.01)
+}
+
+if(multiplethreads == TRUE){
+mc <- getOption("mc.cores", 4)
+PAScollaspe <- parallel::mclapply(overlapid,
+    function(id){
+        idn <- unlist(strsplit(allnames[id],split=","))
+        fix <- apply(rawcount_fix[c(idn,allnames[id]),],2,function(x){ idl <- length(x); y <- (x[-idl]+x[idl]/(idl-1)) ; return(y) })
+        return(fix)},
+    mc.cores = mc)
+
+PAScollaspeMatrix <- c()
+for( i in 1:length(PAScollaspe)){
+    PAScollaspeMatrix <- rbind(PAScollaspeMatrix,PAScollaspe[[i]])
+}
+PAScollaspeMatrix <- Matrix::Matrix(as.matrix( PAScollaspeMatrix ), sparse = TRUE)
+
+rmid <- match(c(rownames(PAScollaspeMatrix),overlapname),rownames(rawcount_fix))
+rawcount_fix <- rawcount_fix[-rmid,]
+rawcount_fix <- rbind(rawcount_fix,PAScollaspeMatrix)
+rawcount_fix <- round(rawcount_fix,0.01)
+}
+
 write.table( as.matrix(rawcount_fix), file = outfile, quote = FALSE, row.names = TRUE, col.names = TRUE, sep = "\t")
 system(paste0("gzip -f ",outfile))
