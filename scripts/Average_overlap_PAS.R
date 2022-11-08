1fbec18307837bcb71040e0c3e4f88d9b7770ce7
guowei li <38876792+liguowei-CAS@users.noreply.github.com>
2022-11-08 14:34:18 +0800

    Update Average_overlap_PAS.R

--- scripts/Average_overlap_PAS.R
+++ scripts/Average_overlap_PAS.R
@@ -15,7 +15,7 @@ if(length(overlapid) == 0){
 	quit(save="no")
 }
 overlapname <- allnames[overlapid]
-multiplethreads <- TRUE
+multiplethreads <- FALSE
 
 if(multiplethreads == FALSE){
 for( id in overlapid ){
@@ -26,26 +26,26 @@ rawcount_fix <- rawcount_fix[-overlapid,]
 rawcount_fix <- round(rawcount_fix,0.01)
 }
 
-if(multiplethreads == TRUE){
-mc <- getOption("mc.cores", 4)
-PAScollaspe <- parallel::mclapply(overlapid,
-    function(id){
-        idn <- unlist(strsplit(allnames[id],split=","))
-        fix <- apply(rawcount_fix[c(idn,allnames[id]),],2,function(x){ idl <- length(x); y <- (x[-idl]+x[idl]/(idl-1)) ; return(y) })
-        return(fix)},
-    mc.cores = mc)
+# if(multiplethreads == TRUE){
+# mc <- getOption("mc.cores", 4)
+# PAScollaspe <- parallel::mclapply(overlapid,
+#     function(id){
+#         idn <- unlist(strsplit(allnames[id],split=","))
+#         fix <- apply(rawcount_fix[c(idn,allnames[id]),],2,function(x){ idl <- length(x); y <- (x[-idl]+x[idl]/(idl-1)) ; return(y) })
+#         return(fix)},
+#     mc.cores = mc)
 
-PAScollaspeMatrix <- c()
-for( i in 1:length(PAScollaspe)){
-    PAScollaspeMatrix <- rbind(PAScollaspeMatrix,PAScollaspe[[i]])
-}
-PAScollaspeMatrix <- Matrix::Matrix(as.matrix( PAScollaspeMatrix ), sparse = TRUE)
+# PAScollaspeMatrix <- c()
+# for( i in 1:length(PAScollaspe)){
+#     PAScollaspeMatrix <- rbind(PAScollaspeMatrix,PAScollaspe[[i]])
+# }
+# PAScollaspeMatrix <- Matrix::Matrix(as.matrix( PAScollaspeMatrix ), sparse = TRUE)
 
-rmid <- match(c(rownames(PAScollaspeMatrix),overlapname),rownames(rawcount_fix))
-rawcount_fix <- rawcount_fix[-rmid,]
-rawcount_fix <- rbind(rawcount_fix,PAScollaspeMatrix)
-rawcount_fix <- round(rawcount_fix,0.01)
-}
+# rmid <- match(c(rownames(PAScollaspeMatrix),overlapname),rownames(rawcount_fix))
+# rawcount_fix <- rawcount_fix[-rmid,]
+# rawcount_fix <- rbind(rawcount_fix,PAScollaspeMatrix)
+# rawcount_fix <- round(rawcount_fix,0.01)
+# }
 
 write.table( as.matrix(rawcount_fix), file = outfile, quote = FALSE, row.names = TRUE, col.names = TRUE, sep = "\t")
 system(paste0("gzip -f ",outfile))
